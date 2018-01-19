2a4d99f6401872aa5d5e56d52c71a7d1ede762fe
Lee Barber <lfb11@alumni.psu.edu>
2018-01-19 15:12:48 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var gettingTheDate = new Date();
+document.getElementById('showingTheDate').innerHTML = gettingTheDate;
+
+var currentHours = gettingTheDate.getHours();
+var currentMinutes = gettingTheDate.getMinutes();
+var currentSeconds = gettingTheDate.getSeconds();
+
+console.log('This is linked')
